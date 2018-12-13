39fdb9888c98b7e09bbb13cfa22aa11a5cbc5f7f
DESKTOP-JOVI9FA\wuxiaohai <dbjinjin0820@qq.com>
2018-12-13 16:38:51 +0800

    项目初始化

---
+++ src/main/java/com/kakasys/flyshare/FlyshareApplication.java
@@ -0,0 +1,14 @@
+package com.kakasys.flyshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyshareApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kakasys/flyshare/FlyshareApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kakasys.flyshare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyshareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
