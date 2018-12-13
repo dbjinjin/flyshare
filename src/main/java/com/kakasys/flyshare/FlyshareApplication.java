7729848cb687550dd9ea9b33d3ff9327a4fe895f
DESKTOP-JOVI9FA\wuxiaohai <dbjinjin0820@qq.com>
2018-12-13 17:14:07 +0800

    no message

--- src/main/java/com/kakasys/flyshare/FlyshareApplication.java
+++ src/main/java/com/kakasys/flyshare/FlyshareApplication.java
@@ -2,13 +2,23 @@ package com.kakasys.flyshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlyshareApplication {
+public class FlyshareApplication extends SpringBootServletInitializer
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlyshareApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FlyshareApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(FlyshareApplication.class);
+    }
 
 }
 
